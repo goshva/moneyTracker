55c02da6443abf3dbcc871d2642cff749e1dbe88
goshva <sha.egor@gmail.com>
2024-03-05 09:29:19 +0000

    sync.sh

--- sync.sh
+++ sync.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/bash
-cd /var/www/bots/moneyTracker
-/usr/bin/git -C /var/www/bots/moneyTracker/ pull && ./git-auto -o -p && /usr/bin/git -C /var/www/bots/moneyTracker push
+git -C /var/www/bots/moneyTracker/ pull && ./git-auto -o -p && /usr/bin/git -C /var/www/bots/moneyTracker push
