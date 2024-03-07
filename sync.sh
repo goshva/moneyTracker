410f2f23eb9d8254e098b3f913d1dba1075e4f15
goshva <sha.egor@gmail.com>
2024-03-07 06:57:44 +0000

    yarik photos

--- sync.sh
+++ sync.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-/usr/bin/git -C /var/www/bots/moneyTracker/ pull && /var/www/bots/moneyTracker/git-auto -o -p && /usr/bin/git -C /var/www/bots/moneyTracker push
+/usr/bin/git -C /var/www/bots/moneyTracker/ pull 
+/var/www/bots/moneyTracker/git-auto -o -p 
+/usr/bin/git -C /var/www/bots/moneyTracker push
